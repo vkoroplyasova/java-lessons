89c434abd6361c3bcec7c62f923ccd25567cece7
rnemykin <nemykinroman@gmail.com>
2017-03-03 14:27:52 +0300

    ~

---
+++ src/main/java/ru/tn/courses/rnemykin/Main.java
@@ -0,0 +1,10 @@
+package ru.tn.courses.rnemykin;
+
+/**
+ * Created by rnemykin on 03.03.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("it works!");
+    }
+}
