fc2611042903af2585828d9c3a20db787214e6d0
rnemykin <nemykin.roman@gmail.com>
2021-10-21 12:24:36 +0300

    ~

--- src/main/java/ru/tn/courses/rnemykin/Main1.java
+++ src/main/java/ru/tn/courses/rnemykin/Main1.java
@@ -3,7 +3,7 @@ package ru.tn.courses.rnemykin;
 /**
  * Created by rnemykin on 03.03.17.
  */
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         System.out.println("it works!");
     }
